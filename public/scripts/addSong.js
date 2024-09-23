a5cfbd60ea0e6993d88f1524ea7f75f359359c90
Traume von Heidelberg <jsd.usis@gmail.com>
2024-09-23 21:37:04 +0200

    I'm a masochist

--- public/scripts/addSong.js
+++ public/scripts/addSong.js
@@ -27,32 +27,22 @@ addSongModal.addEventListener('click', event => {
 // Funkcja do dynamicznego załadowania utworów (aktualizacja listy utworów po dodaniu nowego)
 async function loadTracks() {
 	try {
-		const response = await fetch('/api/tracks') // Pobierz listę utworów z API
-		if (response.ok) {
-			const tracks = await response.json() // Parsowanie odpowiedzi
-			const trackList = document.querySelector('.track-list') // Element listy utworów
-			trackList.innerHTML = '' // Wyczyszczenie aktualnej listy
+		// Wysłanie danych do API
+		const response = await fetch('https://dagankplaylist.onrender.com/api/tracks', {
+			method: 'POST',
+			body: formData,
+		})
 
-			tracks.forEach((track, index) => {
-				const trackElement = document.createElement('li')
-				trackElement.classList.add('track')
-				trackElement.innerHTML = `
-                    <div class="track-main-info">
-                        <span class="track-number">${index + 1}</span>
-                        <i class="play-track-icon fa-solid fa-play" data-src="${track.file}"></i>
-                        <img class="track-image" src="./img/da gank members/me.webp" alt="Da gank member who added this song">
-                        <span class="track-title">${track.title}</span>
-                    </div>
-                    <span class="track-plays">0</span>
-                    <span class="track-duration">${track.duration}</span>
-                `
-				trackList.appendChild(trackElement) // Dodanie utworu do listy
-			})
+		if (response.ok) {
+			const newTrack = await response.json() // Nowy dodany utwór
+			console.log('Utwór został dodany:', newTrack)
+			hideAddSongModal() // Ukryj modal po dodaniu utworu
+			loadTracks() // Odśwież listę utworów po dodaniu nowego
 		} else {
-			console.error('Błąd podczas pobierania listy utworów.')
+			console.error('Błąd podczas dodawania utworu.')
 		}
 	} catch (error) {
-		console.error('Błąd podczas ładowania listy utworów:', error)
+		console.error('Błąd podczas wysyłania formularza:', error)
 	}
 }
 
